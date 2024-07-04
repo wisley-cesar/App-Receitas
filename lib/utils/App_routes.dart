37577e05dc78e076a28e5a0add74b1732eb7277b
Wisley César <wisleycesar32@gmail.com>
2024-07-04 15:34:27 -0300

    Rotas de Navegação

---
+++ lib/utils/App_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const HOME = '/';
+  static const CATEGORIES_MEALS = '/categories-meals';
+  static const MEALS_DETAIL = '/meals-detail';
+  static const SETTING = '/setting';
+}
